32fdfac676831fe1273ada7ffbe6ba622d375564
lumendes10 <lucasmendes2379@gmail.com>
2023-12-11 13:48:55 -0300

    teste

---
+++ var_disciplinas.js
@@ -0,0 +1,24 @@
+var colecao_Diciplinas = [
+{id: 1, nome: "Portugues", carga_horaria: 240},
+{id: 2, nome:"Matemática", carga_horaria:220},
+{id: 3, nome: "História",  carga_horaria: 160},
+{id: 4, nome: "Geografia", carga_horaria: 140},
+{id: 5, nome: "Quimica", carga_horaria: 160},
+{id: 6, nome: "Fisíca", carga_horaria: 150},
+{id: 7, nome: "Ingles", carga_horaria: 120},
+{id: 8, nome: "Educação_fisica", carga_horaria: 130},
+{id: 9, nome: "Biologia", carga_horaria: 110},
+]
+
+console.log(colecao_Diciplinas[3].id);
+console.log(colecao_Diciplinas[3].nome);
+console.log(colecao_Diciplinas[3].carga_horaria);
+
+console.log(colecao_Diciplinas[5].id);
+console.log(colecao_Diciplinas[5].nome);
+console.log(colecao_Diciplinas[5].carga_horaria);
+
+
+console.log(colecao_Diciplinas[7].id);
+console.log(colecao_Diciplinas[7].nome);
+console.log(colecao_Diciplinas[7].carga_horaria);
